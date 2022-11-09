ff68378e6cabcb38f06276ca1d53ce9d16f96551
Eric47123 <108050823+Eric47123@users.noreply.github.com>
2022-11-09 16:47:58 +0800

    pull commit

--- src/main/java/test/GitTest.java
+++ src/main/java/test/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hot-fix change!!");
         System.out.println("hot-fix change");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
