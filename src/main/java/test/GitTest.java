544485ae07fb68f95b88ee4e577ff850f9f1dbc4
LWF <LWF@qq.com>
2022-11-09 12:23:46 +0800

    master change

--- src/main/java/test/GitTest.java
+++ src/main/java/test/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello world");
         System.out.println("second commit");
         System.out.println("hot-fix");
+        System.out.println("hot-fix change");
     }
 }
